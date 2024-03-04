9fac7a9d1485a175fc53e6d4c80740a9c3f21fd7
Sharath <132804379+Sharathhv11@users.noreply.github.com>
2024-03-04 21:45:21 +0530

    Add files via upload
    music player

---
+++ music_player/main.py
@@ -0,0 +1,90 @@
+from music_player import music
+
+class Node:
+    """defining the node structure that contains pointers and data feilds"""
+    def __init__(self,path,name):
+        self.pointer1= None
+        self.pointer2=None
+        self.path = path
+        self.name=name
+        
+class DoubleLinklist:
+    def __init__(self) -> None:
+        self.head = None
+
+    def insert_end(self, path, name):
+        node1 = Node(path, name)
+        if self.head is None:
+            self.head = node1
+            
+        else:
+            current = self.head
+            while (current.pointer2):
+                current=current.pointer2
+            current.pointer2=node1
+            node1.pointer1=current
+        # print(node1.pointer2,node1.pointer1,self.head,sep="\n")
+    
+    def insert_begining(self,path,name):
+        node1 = Node(path, name)
+        if self.head is None:
+            self.head = node1
+        else:
+            self.head.pointer1=node1
+            node1.pointer2=self.head
+            self.head=node1
+    def display(self):
+        if self.head is None:
+            print("no music is added")
+        else :
+            current = self.head
+            while current:
+                print(current.name)
+                current=current.pointer2
+    def search(self,find : str):
+        if self.head is None:
+            print("no music is added")
+        else :
+            current = self.head
+            while current:
+                if(current.name.lower() == find.lower()):
+                    return current.path,current.name
+                else:
+                    current=current.pointer2
+
+    def music_play(self,name):
+        a=self.search(name)[0]
+        print(a)
+        music_obj = music(a)
+        music_obj.play_music()
+
+
+
+def value_input():
+    inp1 = input("Enter song path")
+    inp2 = input("enter alias name for song :")
+    return inp1,inp2
+def main():
+    obj = DoubleLinklist()
+    
+    while (True):
+        inp = int(input("1 - begin 2- end 3-display 4-play_music  5-search_music : "))
+        match(inp):
+            case 1:
+                obj.insert_begining(*value_input())
+            case 2:
+                obj.insert_end(*value_input())
+            case 3:
+                obj.display()
+            case 4:
+                obj.music_play(input("play song name :"))
+            case 5:
+                print(obj.search(input("Find")))
+            case _:
+                print("you have entered a wrong choice!")
+        
+    
+
+
+if __name__=="__main__":
+    main()
---
+++ music_player/music_player.py
@@ -0,0 +1,55 @@
+import pygame as pg
+
+
+class music:
+	"""music class that has various aspect of music player"""
+
+
+	def __init__(self,path) -> None:
+		#initializing the pygame module
+		pg.mixer.init()
+		#loading file to pygame module
+		pg.mixer.music.load(path)
+	
+	def play_music(self):
+		"""this method plays the loaded music"""
+		pg.mixer.music.play()
+		
+	def pause_music(self):
+		"""this method pauses the current playing music"""
+		pg.mixer.music.pause()
+		
+	def unpause_music(self):
+		"""this method unpaused the paused music"""
+		pg.mixer.music.unpause()
+		
+	def stop_music(self):
+		"""this method stops the current music"""
+		pg.mixer.music.stop()
+
+
+
+if (__name__=="__main__"):
+	
+	def main():
+		#creating object for music class y passing file path as constructer argument
+		obj = music("E:\music_player\musics\Arijit Singh  Main Tera Boyfriend.mp3")
+
+		#this block of code manages the music player by playing pausing etc
+		while (True):
+			music_status=input("enter P-play , PA - pause , S - stop , UP - unpause :").lower()
+			
+			if(music_status=="p"):
+				obj.play_music()
+			elif(music_status=="pa"):
+				obj.pause_music()
+				
+			elif(music_status=="up"):
+				obj.unpause_music()
+		
+			elif(music_status=="s"):
+				obj.stop_music()
+			
+			else:
+				print(f" {music_status} you must have entered the wrong choice")
+		main()
